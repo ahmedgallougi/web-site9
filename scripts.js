4bb44ebc0a16747c82ca6403935bf91e27593a3d
ahmedgallougi <98272544+ahmedgallougi@users.noreply.github.com>
2022-04-11 16:14:12 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,43 @@
+var light = document.getElementById('lighbulb');
+
+light.addEventListener('click', function(light){
+light.target.classList.toggle('bulb-on');
+})
+  
+
+
+
+
+
+
+
+// smoothscrolling
+$("#navbar a, .btn").on("click", function(event){
+
+    if(this.hash!=="") {
+        event.preventDefault();
+
+        const hash = this.hash;
+
+        $("html, body").animate({scrollTop: $(hash).offset().top - 100},800);
+}
+});
+// Navbar Opacity
+window.addEventListener("scroll",function () {
+    if(window.scrollY > 150) {
+        document.querySelector("#navbar").style.opacity = 0.9;
+    } else {
+        document.querySelector("#navbar").style.opacity = 1;
+    }
+
+
+
+
+
+
+});
+
+
+
+
+
